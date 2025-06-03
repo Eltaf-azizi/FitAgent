016bd6cac6a7bf645339191bed9a48fc30d7c81c
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-03 19:36:15 +0500

    working on the register function

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ app = Flask(__name__)
 
 # In-memory chat history (just for demo)
 chat_memory = []
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form["name"]
+        age = int(request.form["age"])
+        fitness_level = request.form["fitness_level"]
+        goal = request.form["goal"]
+        equipment = request.form["equipment"]
+
+
